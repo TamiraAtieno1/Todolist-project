99e029228bbf338ce07d2b3552bccf95fc2c778a
CassieRasugu <cassierasugu@gmail.com>
2024-07-16 09:30:52 +0300

    Update addfile.py

--- addfile.py
+++ addfile.py
@@ -1,8 +1,28 @@
-def addTask():
-  task = input("Please enter a task: ")
-  task.append(task)
-  print(f"Task '{task}' added to the list.")
 
-def listTasks():
-  if not tasks:
-    print("All Tasks have been completed,No tasks Available.")
+tasks = [
+  ("Bathing", "2024-07-01"),
+    ("Cooking", "2024-07-01"),
+    ("Eating", "2024-07-01"),
+    ("Buying soap", "2024-07-04"),
+    ("Reading Atomic Habits", "2024-07-05"),
+]
+
+def add_task(name, due_date):
+    """
+    Adds a task to the tasks list with the due date 
+    """
+    tasks.append({"name": name, "due_date": due_date, "completed": False})
+
+def view_tasks():
+    """
+    Displays all tasks in the tasks list.
+    """
+    if not tasks:
+        print("No tasks to display.")
+        return
+    for task in tasks:
+        status = "Completed" if task["completed"] else "Not Completed"
+        print(f"Task: {task['name']}, Due Date: {task['due_date']}, Status: {status}")
+
+
+
