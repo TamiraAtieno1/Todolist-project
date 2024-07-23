65b12a3490011ba851db664b595e6dc337aba2ef
Nkathaglow <muriungiglory0@gmail.com>
2024-07-23 07:20:58 +0300

    Removing some bits from the code

--- delete.py
+++ delete.py
@@ -1,18 +1,7 @@
 tasks = []
 
-# Function to list tasks
-def listTasks():
-    # Check if there are tasks in the list
-    if len(tasks) == 0:
-        print("No tasks found.")
-    else:
-        print("Tasks:")
-        for i, task in enumerate(tasks, start=1):
-            print(f"{i}. {task}")
-
 # Function to delete tasks from the list
 def deleteTask():
-    listTasks()
     try:
         taskToDelete = int(input("Enter the task number to delete: ")) - 1
         # Checks if the task number exists
@@ -30,8 +19,7 @@ def deleteTask():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-if __name__ == "__main__":
-    print("Welcome to my to-do list app:")
+
     
     # Call deleteTask() directly
     deleteTask()
